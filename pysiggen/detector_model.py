e4729bc5fd60042f46e2b47117960e4f5f1878e2
Ben Shanks <bshanks@unc.edu>
2017-05-26 17:02:10 -0400

    import the thing

--- pysiggen/detector_model.py
+++ pysiggen/detector_model.py
@@ -5,6 +5,7 @@ import numpy as np
 import copy, math
 import ctypes
 from scipy import  signal, interpolate, ndimage
+from scipy.stats import skewnorm
 import numbers
 
 from ._pysiggen import Siggen
